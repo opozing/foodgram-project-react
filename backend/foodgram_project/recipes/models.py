d6c229b9f3533ad6672df959ba8264f53665d2ca
opozing <gnom213@yandex.ru>
2021-11-13 02:35:34 +0300

    make docker-compose, nginx.conf, workflow 3 - deploy = ok

--- backend/foodgram_project/recipes/models.py
+++ backend/foodgram_project/recipes/models.py
@@ -19,7 +19,7 @@ class Tag(models.Model):
 
 
 class Ingredient(models.Model):
-    name = models.CharField('Название', max_length=50)
+    name = models.CharField('Название', max_length=100)
     measurement_unit = models.CharField('Единицы измерения', max_length=200)
 
     class Meta:
