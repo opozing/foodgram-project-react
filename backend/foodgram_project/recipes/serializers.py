56626e9ddb4b915ef8db7b52b6ecb48e02dd0723
opozing <gnom213@yandex.ru>
2021-11-16 00:42:44 +0300

    repair update, imports

--- backend/foodgram_project/recipes/serializers.py
+++ backend/foodgram_project/recipes/serializers.py
@@ -2,8 +2,8 @@ from django.contrib.auth import get_user_model
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
-from users.serializers import ReUserSerializer
 
+from users.serializers import ReUserSerializer
 from .models import (FavoriteRecipe, Ingredient, Recipe, RecipeIngredient,
                      ShoppingCart, Tag)
 
